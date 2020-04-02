564ce4e9ffee968e70f2c6552e5b6857b3d2e67f
miclau13 <miclau2004@gmail.com>
2020-04-02 10:06:55 +0800

    install lightbox

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,3 +1,7 @@
+import {Dimensions} from 'react-native';
+
 export const buttonPrimaryColor = '#2196f3';
 export const headerPrimaryColor = '#3CB371'; // medium sea green
 
+export const windowWidth = Dimensions.get('window').width;
+export const windowHeight = Dimensions.get('window').height;
