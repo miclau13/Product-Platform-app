923b88c3c56acbecdce728db6736071d2b7ca600
miclau13 <miclau2004@gmail.com>
2020-05-18 23:52:12 +0800

    update endpoint

--- src/api/ratingProduct.ts
+++ src/api/ratingProduct.ts
@@ -1,6 +1,6 @@
 const ratingProduct = async (id: string, rating: number) => {
-  await fetch(`http://192.168.0.106:5000/products/rating/${id}`, {
-  // await fetch(`https://miclo1.azurewebsites.net/products/rating/${id}`, {
+  // await fetch(`http://192.168.0.106:5000/products/rating/${id}`, {
+  await fetch(`https://miclo1.azurewebsites.net/products/rating/${id}`, {
     method: 'post',
     headers: {
       Accept: 'application/json',
