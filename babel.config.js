df19f53aecf40800a26515aaae618a43bf9a9725
miclau13 <miclau2004@gmail.com>
2020-08-10 15:06:48 +0800

    add global path

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,25 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            "@assets": './assets',
+            "@api": './src/api',
+            "@components": './src/components',
+            "@context": './src/context',
+            "@hooks": './src/hooks',
+            "@languages": './src/languages',
+            "@navigator": './src/navigator',
+            "@screens": './src/screens',
+            "@styles": './src/styles',
+          }
+        }
+      ]
+    ]
   };
 };
